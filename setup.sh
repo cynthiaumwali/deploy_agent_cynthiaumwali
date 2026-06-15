9995eb72d0506ce63a46550114dec46f24e5fe59
ucynthia13 <cynthiaumwali3@gmail.com>
2026-06-15 19:02:36 +0200

    refactor: improve comments for tasks separation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function setup() {
+    #1.create parent directory and subdirectories with respective files
     read -p "Enter version name/number: " version
     parent_directory="attendance_tracker_${version}"
-    #check if directory exists
     if [ -d "attendance_tracker_${version}" ]
     then
         echo "Directory already exists. Please choose a different version name/number."
@@ -23,6 +23,7 @@ function setup() {
 
 	    read -p "Do you want to update attendance thresholds? [yes/no]: " choice
 
+    #2. Dynamic threshold configuration
         if [[ "$choice"=="yes" ]]
         then
             read -p "Enter new threshold value for Warning: " warning_value
@@ -37,7 +38,10 @@ function setup() {
             echo "Attendance threshold values remain unchanged. Proceeding with next step"
         fi
 
+    #3. Handle SIGINT [Ctrl+C] using the TRAP command
         trap 'echo "Process Interrupted. Bundling the current project state..."; tar -czf "${parent_directory}_archive.tar.gz" "$parent_directory" && rm -r "$parent_directory"; exit 1' SIGINT
+
+    #4. Environment validation [Checking if python3 is installed & verifying application directory structure]
 	    echo "Checking if python3 exists..."
 
 	    if command -v python3 >/dev/null 
