5ebf6123bbf4d2be1bcb9c18ecd6c5abc474bca9
ucynthia13 <cynthiaumwali3@gmail.com>
2026-06-16 14:28:41 +0200

    fix: improve output messages and validate attendance threshold inputs

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ parent_directory=""
     if [[ -d "$parent_directory" ]]
     then
         tar -czf "${parent_directory}_archive.tar.gz" "$parent_directory" && rm -rf "$parent_directory"
-        echo -e "\nProject state bundled into ${parent_directory}_archive.tar.gz. Original directory is deleted."
+        echo -e "\nProject state bundled into ${parent_directory}_archive.tar.gz. Original directory has been deleted."
     else
         echo -e "\nParent directory not set yet. No bundling performed. Exiting.."
     fi
@@ -32,7 +32,7 @@ function setup() {
 
     parent_directory="attendance_tracker_${version}"
 
-        mkdir -p "$parent_directory" "$parent_directory/Helpers" "$parent_directory/reports"
+    mkdir -p "$parent_directory" "$parent_directory/Helpers" "$parent_directory/reports"
 
         if [[ $? -ne 0 ]]
         then
@@ -72,33 +72,48 @@ function setup() {
             exit 1
         fi
 
-        echo "Parent directory and subdirectories created successfully with respective files."
+        echo -e "Parent directory and subdirectories created successfully with respective files.\n"
 
         #2. Dynamic threshold configuration
 
-	    read -p "Do you want to update attendance thresholds? [yes/no]: " choice
+        while true
+        do
+            read -p "Do you want to update the default attendance threshold values? (yes/no): " choice
+            if [[ "$choice" == "yes" || "$choice" == "no" || "$choice" == "YES" || "$choice" == "NO" ]]
+            then
+                break
+            else
+                echo -e "Invalid input. Please enter 'yes' or 'no'.\n"
+            fi
+        done
 
         if [[ "$choice" == "yes" || "$choice" == "YES" ]]
         then
+
+        while true
+        do
             read -p "Enter new threshold value for Warning: " warning_value
             read -p "Enter new threshold value for Failure: " failure_value
 
-            if [[ "$warning_value" =~ ^[+-]?[0-9]*\.?[0-9]+$ || "$failure_value" =~ ^[+-]?[0-9]*\.?[0-9]+$ ]]
-            then
-                echo "Invalid input. Attendance threshold values must be numbers."
-                exit 1
-            fi
+            if [[ ! "$warning_value" =~ ^[+]?[0-9]*\.?[0-9]+$ || ! "$failure_value" =~ ^[+-]?[0-9]*\.?[0-9]+$ ]]
+                then
+                    echo -e "Invalid input. Attendance threshold values must be numbers.\n"
+            elif [[ "$warning_value" -gt 100 && "$failure_value" -gt 100 ]]
+                then
+                    echo -e "Invalid input. Attendance threshold values must be less than or equal to 100.\n"
+            else 
+                    break
+                fi
+        done
 
-            sed -i "s/75/$warning_value/g" "$parent_directory/Helpers/config.json"
-            sed -i "s/50/$failure_value/g" "$parent_directory/Helpers/config.json"
+        sed -i "s/75/$warning_value/g" "$parent_directory/Helpers/config.json"
+        sed -i "s/50/$failure_value/g" "$parent_directory/Helpers/config.json"
 
-            echo "Threshold values updated"
-            
+        echo -e "Attendance threshold values updated.\n"
 
         elif [[ "$choice" == "no" || "$choice" == "NO" ]]
         then
-            echo "Attendance threshold values remain unchanged. Proceeding with next step"
-
+            echo -e "Attendance threshold values remain unchanged. Proceeding with next step.\n"
         else
             echo "You entered an invalid choice. Please enter 'yes' or 'no'."
             exit 1
@@ -116,11 +131,10 @@ function setup() {
 
         if [[ -d "$parent_directory" ]] && [[ -f "$parent_directory/attendance_checker.py" ]] && [[ -f "$parent_directory/Helpers/assets.csv" ]] && [[ -f "$parent_directory/Helpers/config.json" ]] && [[ -f "$parent_directory/reports/reports.log" ]]
         then
-            echo "Application directory structure is correct"
+            echo "Application directory structure is correct!"
         else
-            echo "Application directory structure is not correct"
+            echo "Application directory structure is not correct! Please validate the setup and try again."
         fi
-    fi
 }
 
 setup
